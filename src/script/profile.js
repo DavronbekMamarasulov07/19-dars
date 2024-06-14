a7291ba2a17b9b2beed0efcba70153a1d40c4bfc
dev-davronbek <davronbekmamarasulov07@gmail.com>
2024-06-14 21:30:51 +0500

    adedd

--- src/script/profile.js
+++ src/script/profile.js
@@ -1,26 +1,97 @@
+
+
 import axios from "../api/axios.js";
-// import { handleLoading } from "../utils/index.js";
-import { renderUser } from "./render.js";
-
-
-
-// const $container = document.querySelector("#container");
 
 const loadProfileData = async () => {
-    
-    try{
-        // handleLoading(true, $container)
+    try {
         const response = await axios("/users/profile");
-        console.log(response.data);
-        renderUser(response.data);
+        const userData = response.data;
+
+        renderUser(userData);
+    } catch (error) {
+        console.log(error);
     }
-    catch(error){
-        console.log(error)
+};
+
+
+
+
+
+let userid
+const renderUser = (userData) => {
+    const $userName = document.querySelector(".user-name");
+    const $userEmail = document.querySelector(".user-email  ");
+
+    $userName.innerText = userData.name;
+    $userEmail.innerText = userData.email;
+    userid = userData._id
+    console.log(userid);
+};
+
+const updateUser = async (userId, newData) => {
+    try {
+        const response = await axios.put(`/users/${userId}`, newData);
+        const updatedData = response.data;
+
+        renderUser(updatedData);
+    } catch (error) {
+        console.error('Foydalanuvchi ma\'lumotlarini yangilashda xato:', error.message);
     }
-    finally{
-        // handleLoading(false, $container)
-    }
-}
+};
+
+
+
+const $pencilName = document.querySelector(".pencil-name");
+$pencilName.addEventListener("click", (event) => {
+    event.preventDefault(); 
+
+    const $userNameUpdate = document.querySelector(".name-update");
+    const $userNameShow = document.querySelector(".name-show");
+    $userNameUpdate.classList.add("show");
+    $userNameShow.classList.add("hide");
+});
+const $pencilEmail = document.querySelector(".pencil-email");
+$pencilEmail.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const $userEmailUpdate = document.querySelector(".email-update");
+    const $userEmailShow = document.querySelector(".email-show");
+    $userEmailUpdate.classList.add("show");
+    $userEmailShow.classList.add("hide");
+});
+
+// Ismni yangilash tugmasi
+const $nameUpdateBtn = document.querySelector(".name-update-btn");
+$nameUpdateBtn.addEventListener("click", async (event) => {
+    event.preventDefault(); 
+
+    const $userNameUpdate = document.querySelector(".name-update");
+    const $userNameShow = document.querySelector(".name-show");
+    $userNameUpdate.classList.remove("show");
+    $userNameShow.classList.remove("hide");
+
+    const userId = userid; 
+    const newName = document.querySelector(".user-name-value").value;
+    const newData = { name: newName };
+
+    await updateUser(userId, newData);
+});
+
+const $emailUpdateBtn = document.querySelector(".email-update-btn");
+$emailUpdateBtn.addEventListener("click", async (event) => {
+    event.preventDefault(); 
+    const $userEmailUpdate = document.querySelector(".email-update");
+    const $userEmailShow = document.querySelector(".email-show");
+    $userEmailUpdate.classList.remove("show");
+    $userEmailShow.classList.remove("hide");
+
+    const userId = userid; 
+    const newEmail = document.querySelector(".user-email-value").value;
+    const newData = { email: newEmail };
+    
+
+    await updateUser(userId, newData);
+});
 
 loadProfileData();
 
